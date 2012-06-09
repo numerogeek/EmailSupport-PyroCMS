010724579dd82e3cd3f0abea1bec19b2bd95548b
Christian Giupponi <christian.giupponi@stilogo.it>
2012-06-09 11:03:51 +0300

    Update master

--- views/form.php
+++ views/form.php
@@ -16,7 +16,7 @@
 									<div class="input"><input type="text" name="lastname" value="<?php echo set_value('name'); ?>"></div>
 								</li>
 								<li class="even">									
-									<label for="message"><?php echo lang('label_message'); ?>:</label>
+									<label for="message"><?php echo lang('label_message'); ?>: <span>*</span></label>
 									<div class="input"><textarea name="message" cols="20" rows="8" class="wysiwyg-simple"><?php echo set_value('message'); ?></textarea></div>
 								</li>								
 							</ul>
@@ -24,7 +24,7 @@
 					</div>
 				<div class="buttons">
 		    		<button type="submit" name="btnAction" value="invia" class="btn blue">
-						<span>Invia richiesta</span>
+						<span><?php echo lang('send_request'); ?></span>
 					</button>
 				</div>
 	<?php
