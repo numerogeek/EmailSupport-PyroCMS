abd4a63faac139efbdd037272d208d99d442febe
Christian Giupponi <christian.giupponi@stilogo.it>
2012-06-09 11:06:12 +0300

    Update master

--- language/english/supporto_lang.php
+++ language/english/supporto_lang.php
@@ -2,16 +2,18 @@
 /**
  * 
  * @author      Christian Giupponi STILOGO
- * @link		http://www.stilogo.it
+ * @link  	http://www.stilogo.it
  * @package 	PyroCMS
  * @subpackage  supporto
  * @category	module
  * @license     FREE
  */
-  $lang['label_support'] = "Contact form";
+  $lang['label_support'] 	= "Contact form";
+  $lang['label_message']	= "Message";
+  $lang['label_name']	 	= "Name";
+  $lang['label_lastname'] 	= "Lastname";
   
-  $lang['label_message'] = "Message";
-  $lang['label_name'] = "Name";
-  $lang['label_lastname'] = "Lastname";
-  $lang['success_email'] = "Your support request has been sent.";
-  $lang['error_email'] = "Unable to send your support request.";
+  $lang['success_email'] 	= "Your support request has been sent.";
+  $lang['error_email'] 		= "Unable to send your support request.";
+
+  $lang['send_request']		= "Send request";
